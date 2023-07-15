b44db1799ede7347842da22e3e4d578081a40f9a
Alexander Walters <1781587+tritium21@users.noreply.github.com>
2023-07-15 10:14:25 -0400

    test changes didn't want to stage with the rest

--- test/demo.py
+++
@@ -1,9 +0,0 @@
-import requests
-import platform
-
-
-def main():
-    print(platform.architecture())
-    print(platform.python_version())
-    print(requests.__version__)
-    print("Hello World")
