8c9a09b45b4c406720786edb0ae1f7a45c29edff
Alexander Walters <1781587+tritium21@users.noreply.github.com>
2023-07-19 14:47:08 -0400

    build system tweaking

--- build.bat
+++ build.bat
@@ -1,4 +1,11 @@
 @echo off
-env\Scripts\python -m build -w -C="--build-option=--plat win32"
-env\Scripts\python -m build -w -C="--build-option=--plat win_amd64"
-env\Scripts\python -m build -s
+if exist env\Scripts\python.exe (
+    env\Scripts\python.exe -m build -w -C="--build-option=--plat win32"
+    env\Scripts\python.exe -m build -w -C="--build-option=--plat win_amd64"
+    env\Scripts\python.exe -m build -s
+) else (
+    echo Please run:
+    echo    py -m venv env
+    echo    env\Scripts\python.exe -m pip install --upgrade pip setuptools build
+    echo And try again
+)
